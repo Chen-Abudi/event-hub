e87c1fd05cf5c89d8a79aa1e2e314db46660dbba
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-05-26 11:22:15 +0300

    feat: Add loader to fetch event data

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -4,14 +4,14 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
 import { fetchEvent, queryClient, updateEvent } from "../../utils/http.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+// import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
   const navigate = useNavigate();
   const params = useParams();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
@@ -49,13 +49,13 @@ export default function EditEvent() {
 
   let content;
 
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
+  // if (isPending) {
+  //   content = (
+  //     <div className="center">
+  //       <LoadingIndicator />
+  //     </div>
+  //   );
+  // }
 
   if (isError) {
     content = (
@@ -90,4 +90,11 @@ export default function EditEvent() {
   }
 
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader({ params }) {
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
+  });
 }
